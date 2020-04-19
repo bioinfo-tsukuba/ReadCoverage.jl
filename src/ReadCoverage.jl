d6b8d90c98ff33bdfd7be585dc16c856f2273073
Haruka Ozaki <harukao.cb@gmail.com>
2020-04-19 11:32:58 +0900

    Change module name

--- src/ReadCoverage.jl
+++ src/ReadCoverage.jl
@@ -1,12 +1,18 @@
+module ReadCoverage
+
 using GenomicFeatures
 using BioAlignments
 using DelimitedFiles
 using Printf
+using Plots
+using PyPlot
+pyplot()
 
 include("relative_genebodycoverage.jl")
 include("readcovearge_bam.jl")
 include("absolute_genebodycoverage.jl")
 include("load_transcript.jl")
 include("plot.jl")
+include("utils.jl")
 
-
+end # module
