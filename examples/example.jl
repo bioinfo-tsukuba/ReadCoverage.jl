e92ae468d6f26d4a40b7061f791c8d6fb8155f7c
Haruka Ozaki <harukao.cb@gmail.com>
2020-04-20 11:19:08 +0900

    arrange examples

---
+++ examples/example.jl
@@ -0,0 +1,28 @@
+using ReadCoverage
+
+path_bam = "examples/RamDA_72h_A09.uniq.q40.bam"
+path_bed12 = "examples/gencode.vM15.primary_assembly.annotation.protein_coding.head.bed"
+out_prefix = "examples/out/RamDA_72h_A09.uniq.q40"
+
+
+################################
+# Absolute genebody coverage
+abcov = absolute_genebodycoverage(path_bam, path_bed12)
+abcov = absolute_genebodycoverage(path_bam, path_bed12, output_prefix=out_prefix, bin_size=100)
+plot_absolute_coverage(abcov)
+
+
+################################
+# Absolute genebody coverage
+relcov = relative_genebodycoverage(path_bam, path_bed12);
+
+max_depth = 0
+relcov = relative_genebodycoverage(path_bam, path_bed12, output_prefix=out_prefix, max_depth=max_depth);
+plot_relative_coverage(relcov)
+
+############
+chrom = "chr19"
+leftpos = 3205000
+rightpos = 3207000
+cov = readcoverage_bam(path_bam, chrom, leftpos, rightpos)
+plot_read_coverage(cov)
