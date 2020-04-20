d77e4078dfa6cb85077813036877ad2900bb20ab
Haruka Ozaki <harukao.cb@gmail.com>
2020-04-20 11:44:55 +0900

    add CLI (using Fire.jl)

---
+++ cli/run.jl
@@ -0,0 +1,24 @@
+using Fire
+using ReadCoverage
+
+"""
+Calculates relative gene body coverage from a BAM file.
+(Almost same as genebody_coverage.py in RSeQC.)
+"""
+@main function relcov(path_bam::String, path_bed12::String, out::String; max_depth::Int=0)
+    relative_genebodycoverage(path_bam, path_bed12, output_prefix=out, max_depth=max_depth);
+end
+
+"""
+Cacluates absolute gene body coverage from a BAM file
+"""
+@main function abcov(path_bam::String, path_bed12::String, out::String; bin_size::Int=100)
+    absolute_genebodycoverage(path_bam, path_bed12, output_prefix=out, bin_size=bin_size);
+end
+
+"""
+Cacluates absolute gene body coverage from a BAM file
+"""
+@main function coverage(path_bam::String, chrom::String, leftpos::Int64, rightpos::Int64; out::String)
+    readcoverage_bam(path_bam, chrom, leftpos, rightpos, output_prefix=out);
+end
