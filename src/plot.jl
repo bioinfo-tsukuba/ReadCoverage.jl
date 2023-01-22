091d3a2e151bc1865c8d86504220cb731b85bdb0
yuifu <harukao.cb@gmail.com>
2023-01-23 02:01:21 +0900

    Avoid displaying plots

--- src/plot.jl
+++ src/plot.jl
@@ -7,6 +7,7 @@ Plot the absolute gene body coverage.
 """
 function plot_absolute_coverage(abcov::Array{Float64,1}; out_path="")
     x = collect(1:length(abcov)) .* 100
+    ENV["GKSwstype"]="nul"
     p = Plots.plot(x, abcov, xlabel = "Distance from the \$3^\\prime\$ end [bp] (\$5^\\prime\$ -> \$3^\\prime\$)", label = "", title = "", xflip=true);
     if out_path != ""
         Plots.savefig(out_path)
@@ -23,6 +24,7 @@ Plot the relative gene body coverage.
 """
 function plot_relative_coverage(relcov::Array{Float64,1}; out_path="")
     x = collect(1:length(relcov)) .* 100
+    ENV["GKSwstype"]="nul"
     p = Plots.plot(1:100,  (relcov.-minimum(relcov))./(maximum(relcov)-minimum(relcov)), 
             xlabel = "Gene body percentile (\$5^\\prime\$ -> \$3^\\prime\$)", label = "", title = "");
     if out_path != ""
@@ -40,6 +42,7 @@ Plot read coverage.
 """
 function plot_read_coverage(cov::Array{Int64,1}; out_path="")
     x = collect(1:length(cov)) .* 100
+    ENV["GKSwstype"]="nul"
     p = Plots.plot(x, cov, xlabel = "Genomic coordinate [bp]", label = "", title = "");
     if out_path != ""
         Plots.savefig(p, out_path)
